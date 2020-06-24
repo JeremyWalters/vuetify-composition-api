53595900f8207f65cb54344e81a01e17e740da1d
JeremyW <walterjc21@gmail.com>
2020-06-24 11:15:41 +0200

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  props: { msg: String },
+  setup() {
+    return {};
+  }
+});
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue typescript composition api app" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+});
+</script>
